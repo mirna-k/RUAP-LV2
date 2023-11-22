b3f373dbe41a406c28e2265337e498f6cf304d89
mirna-k <mirna_krpic@hotmail.com>
2023-11-22 01:48:24 +0100

    Task 1

---
+++ Begin/ContactMenager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContactMenager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
