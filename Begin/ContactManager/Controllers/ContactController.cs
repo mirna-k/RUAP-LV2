982ffb5b4f9b11e29967fbe2f13e7dde74c3ab12
mirna-k <mirna_krpic@hotmail.com>
2023-11-22 16:02:20 +0100

    Resolve issues

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using ContactManager.Services;
+using ContactManager.Models;
+using System.Web.Http;
+using System.Net.Http;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+    }
+}
